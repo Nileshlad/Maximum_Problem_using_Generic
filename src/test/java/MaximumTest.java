1bd31c23c7ca45a4dfd727d72cce604cab4f92c3
Nileshlad <ladnilesh1111@gmail.com>
2020-03-16 18:13:27 +0530

    Added: To check the possition float value

--- src/test/java/MaximumTest.java
+++ src/test/java/MaximumTest.java
@@ -39,5 +39,26 @@ public class MaximumTest {
         findMaximum.findMaximumFloat(1.2f,2.2f,3.3f);
         Assert.assertNotNull(findMaximum);
     }
+
+    //TEST CASE 2.2
+    @Test
+    public void givenThreeFloat_WhenMaximumFoundAtPosition1_ThenReturnMaximum() {
+        Float maximumFloat = findMaximum.findMaximumFloat(1.1f, 2.2f, 3.3f);
+        Assert.assertEquals((Float) 3.3f, maximumFloat);
+    }
+
+    //TEST CASE 2.3
+    @Test
+    public void givenThreeFloat_WhenMaximumFoundAtPosition2_ThenReturnMaximum() {
+        Float maximumFloat = findMaximum.findMaximumFloat(3.3f, 2.2f, 1.1f);
+        Assert.assertEquals((Float) 3.3f, maximumFloat);
+    }
+
+    //TEST CASE 2.4
+    @Test
+    public void givenThreeFloat_WhenMaximumFoundAtPosition3_ThenReturnMaximum() {
+        Float maximumFloat = findMaximum.findMaximumFloat(3.3f, 2.2f, 1.1f);
+        Assert.assertEquals((Float) 3.3f, maximumFloat);
+    }
 }
 

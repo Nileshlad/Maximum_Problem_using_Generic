1536d6d70cce1548274cd7739863d7d2ef7560ab
Nileshlad <ladnilesh1111@gmail.com>
2020-03-16 13:00:40 +0530

    Added: TC-1.1 To check array object value

--- src/test/java/MaximumTest.java
+++ src/test/java/MaximumTest.java
@@ -1,3 +1,13 @@
-public class MaximumTest {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class MaximumTest {
+    //TEST CASE 1.1
+    @Test
+    public void givenValue_ObjectNotNull_True() {
+        Maximum maximum = new Maximum();
+        maximum.findMaximumInteger();
+        Assert.assertNotNull(maximum);
+    }
 }
+

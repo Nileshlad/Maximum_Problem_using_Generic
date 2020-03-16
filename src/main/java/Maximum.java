407b39f36e42cb5f61437e277d48171a7594dab7
Nileshlad <ladnilesh1111@gmail.com>
2020-03-16 17:55:02 +0530

    Added: To find maximum float value

--- src/main/java/Maximum.java
+++ src/main/java/Maximum.java
@@ -10,6 +10,15 @@ public class Maximum
         return maximumValue;
     }
 
+    public static Float findMaximumFloat(Float oneValue, Float twoValue, Float threeValue) {
+        Float maximumValue = oneValue;
+        if (oneValue.compareTo(maximumValue) > 0)
+            maximumValue = twoValue;
+        if (threeValue.compareTo(maximumValue) > 0)
+            maximumValue = threeValue;
+        return maximumValue;
+    }
+
     //TO MAIN METHOD AND CREATE OBJECT AND OBJECT CALL
     public static void main(String[] args) {
         System.out.println("WELCOME TEST MAXIMUM AND GENERIC");

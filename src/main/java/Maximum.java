9f35dbb8ce28a3c9b28e8f19a6c9c0d083535a13
Nileshlad <ladnilesh1111@gmail.com>
2020-03-16 12:54:55 +0530

    Added: To given three interge array value and check to maximum value

--- src/main/java/Maximum.java
+++ src/main/java/Maximum.java
@@ -3,15 +3,14 @@ public class Maximum
     //TO METHOD
     public void findMaximumInteger()
     {
+        //To use array
         int[] array = new int[]{10,20,30};
         int currentMax = array[0];
         for(int index=1;index<=array.length-1;index++){
             if(currentMax < array[index]){
                 currentMax = array[index];
-
             }
         }
-
         System.out.println("Maximum value is "+ currentMax);
     }
 
